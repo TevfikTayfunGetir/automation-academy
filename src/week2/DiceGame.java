a7901497da3f51166ad470cb1b34ace67b23285d
tevfik.tayfun <tevfik.tayfun@getir.com>
2021-09-16 15:29:22 +0300

    Last Update

--- src/week2/DiceGame.java
+++ src/week2/DiceGame.java
@@ -1,6 +1,8 @@
 package week2;
 
+import java.io.InputStream;
 import java.util.Random;
+import java.util.Scanner;
 
 public class DiceGame {
 
@@ -8,28 +10,41 @@ public class DiceGame {
     {
 
         int diceSum = 0;
-        while( diceSum <= 50)
-        {
-            System.out.println("Rolling the die");
+        Scanner scanner = new Scanner(System.in);
+        while( diceSum <= 50) {
 
-            Random random = new Random();
-            int die = random.nextInt(6) + 1;
+            System.out.println("Your money is " + diceSum);
 
-            if(die == 1)
-            {
-                System.out.println("Game is over. You have rolled 1 or 2");
-                break;
+
+            System.out.println("Wanna continue? ");
+
+            String s = scanner.nextLine();
+
+            if (s.equals("Y")){
+                System.out.println("Rolling the die");
+
+                Random random = new Random();
+                int die = random.nextInt(6) + 1;
+
+                if (die == 1 || die == 2) {
+                    System.out.println("Game is over. You have rolled 1 or 2");
+                    break;
+                } else {
+                    diceSum = diceSum + die;
+                    System.out.println("Current sum is: " + diceSum);
+                }
+
             }
 
-            else {
-                diceSum = diceSum + die;
-                System.out.println("Current sum is: " + diceSum);
+            else if(s.equals("N")){
+                System.out.println("Game Over. Your money is: " + diceSum);
             }
 
         }
         if(diceSum >= 50)
         System.out.println("Congratz you have earned " + diceSum + " amount of money");
 
+        scanner.close();
 
     }
 
